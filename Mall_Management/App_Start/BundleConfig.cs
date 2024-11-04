95fe0ddd08ea59af3f0325e574e0ea6afc0258f9
Duc112233 <giaduc618@gmail.com>
2024-11-04 18:07:13 +0700

    Upload

---
+++ Mall_Management/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+public class BundleConfig
+{
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+        // Bundle cho CSS
+        bundles.Add(new StyleBundle("~/Content/css").Include(
+                  "~/Content/bootstrap.css",
+                  "~/Content/custom.css"));
+
+        // Bundle cho JavaScript
+        bundles.Add(new ScriptBundle("~/bundles/customScript").Include(
+                  "~/Scripts/script.js"));
+
+        bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                  "~/Scripts/bootstrap.js"));
+
+        BundleTable.EnableOptimizations = true;
+    }
+}
---
+++ Mall_Management/App_Start/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+public class RouteConfig
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        // Route dành cho Home controller
+        routes.MapRoute(
+            name: "HomeRoute",
+            url: "Home/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );
+    }
+}
